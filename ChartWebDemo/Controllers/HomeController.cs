d45fceabc5cd6ad592b103b6276b0d09555419e2
Sarthak Malik <malik@sarthak.ca>
2018-09-07 20:12:37 -0400

    Added boilerplate MVC web application

---
+++ ChartWebDemo/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ChartJSCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using ChartWebDemo.Models;
+
+namespace ChartWebDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Chart chart = new Chart();
+
+            chart.Type = "line";
+
+            ChartJSCore.Models.Data data = new ChartJSCore.Models.Data();
+            data.Labels = new List<string>() { "January", "February", "March", "April", "May", "June", "July" };
+
+            LineDataset dataset = new LineDataset()
+            {
+                Label = "My First dataset",
+                Data = new List<double>() { 65, 59, 80, 81, 56, 55, 40 },
+                Fill = false.ToString(),
+                LineTension = 0.1,
+                BackgroundColor = "rgba(75, 192, 192, 0.4)",
+                BorderColor = "rgba(75,192,192,1)",
+                BorderCapStyle = "butt",
+                BorderDash = new List<int> { },
+                BorderDashOffset = 0.0,
+                BorderJoinStyle = "miter",
+                PointBorderColor = new List<string>() { "rgba(75,192,192,1)" },
+                PointBackgroundColor = new List<string>() { "#fff" },
+                PointBorderWidth = new List<int> { 1 },
+                PointHoverRadius = new List<int> { 5 },
+                PointHoverBackgroundColor = new List<string>() { "rgba(75,192,192,1)" },
+                PointHoverBorderColor = new List<string>() { "rgba(220,220,220,1)" },
+                PointHoverBorderWidth = new List<int> { 2 },
+                PointRadius = new List<int> { 1 },
+                PointHitRadius = new List<int> { 10 },
+                SpanGaps = false
+            };
+
+            data.Datasets = new List<Dataset>();
+            data.Datasets.Add(dataset);
+
+            chart.Data = data;
+
+            ViewData["chart"] = chart;
+
+            return View();
+        }
+    }
+}
